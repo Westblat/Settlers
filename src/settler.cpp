1d9d7c58c7bdf32dd10ffbc68c1a2aa8e5bf161b
Tarmo <Tarmo.nieminen24@gmail.com>
2017-11-22 17:58:55 +0200

    [FIX] Fixed more typos in settler class

--- src/settler.cpp
+++ src/settler.cpp
@@ -1,12 +1,12 @@
 #include <string>
-#include "settler.h"
 #include <algorithm>
+#include "settler.h"
 
 Settler::Settler(std::string name, std::string task) : name(name), task(task) {
 	inventory.second = 5;
 }
 
-Settler::addItem(int name, int size) {
+void Settler::addItem(int name, int size) {
 	if (inventory.second - size > 0)
 	{
 		inventory.first.push_back(name);
@@ -14,10 +14,10 @@ Settler::addItem(int name, int size) {
 	}
 }
 
-Settler::removeItem(int name, int size) {
+void Settler::removeItem(int name, int size) {
 	std::vector<Item>::iterator it = find(inventory.first.begin(), inventory.first.end(), name);
 	inventory.first.erase(it);
 	inventory.second = inventory.second + size;
 }
 
-~Settler::Settler() { }
+Settler::~Settler() { }
