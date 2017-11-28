7db62c46e6a157839314777453a53cb72617f751
Tarmo <tarmo.nieminen24@gmail.com>
2017-11-28 19:30:41 +0200

    [IMP] Added more classes and variables to settler

--- src/settler.cpp
+++ src/settler.cpp
@@ -1,10 +1,17 @@
+
+
 #include "settler.h"
 #include <algorithm>
 #include <iostream>
 
+// Constructor take the name of the settler and sets the values for max inventory size, max hp, (current) hp and playerControlled to true
 Settler::Settler(std::string name) : name(name) {
 	inventory.second = 5;
+	hp = 10;
+	maxHp = 10;
+	playerControlled = true;
 }
+
 
 // These functions take no parameters and return the wanted variable
 std::string Settler::getName() { return name; }
@@ -33,9 +40,9 @@ bool Settler::addItem(int item) {
 	}
 }
 
-// Function that deletes a given item (int) from inventory vector assuming the inventory isn't empty (vector.size != 0)
+// Function that deletes a given item (int) from inventory vector assuming the inventory isn't empty (vector.size != 0) and that given item is in the inventory
 bool Settler::removeItem(int item) {
-	if (inventory.first.size() != 0)
+	if ((inventory.first.size() != 0) && (std::find(inventory.first.begin(), inventory.first.end(), item) != inventory.first.end()))
 	{
 		std::vector<int>::iterator it = std::find(inventory.first.begin(), inventory.first.end(), item);
 		inventory.first.erase(it);
@@ -49,18 +56,65 @@ bool Settler::removeItem(int item) {
 
 int Settler::getHP() { return hp; }
 
-// Function that updates settlers hp to a given int value and return true or false based on whether the settler died or not (if hp is below 0, settler died)
-bool Settler::setHP(int newHp)
+// Function that adds a given value to the settlers hp and returns the health the settler has after the addition, if the health were to go over the maximum amount current hp is set to maxHp
+int Settler::addHP(int newHp)
 {
-	if (newHp > 0)
+	if (hp + newHp < maxHp)
 	{
-		hp = newHp;
+		hp = hp + newHp;
+		return hp;
+	}
+	else
+	{
+		hp = maxHp;
+		return hp;
+	}
+}
+
+// Function that removes a given value from the settlers hp and returns the health the settler has after the addition, if the health were to go below 0 it is set to 0
+int Settler::removeHP(int newHp)
+{
+	if (hp - newHp > 0)
+	{
+		hp = hp - newHp;
+		return hp;
+	}
+	else
+	{
+		hp = 0;
+		return hp;
+	}
+}
+
+// Adds given coordinates to path
+void Settler::pushPath(int x, int y)
+{
+	std::pair<int, int> newPath = std::make_pair(x, y);
+	path.push(newPath);
+}
+
+// Returns path
+std::queue <std::pair<int, int>> Settler::getPath() { return path; }
+
+// Sets action delay to the given value
+void Settler::setDelay(int delay) { actionDelay = delay; }
+
+// Reduces the delay by 1, until the delay is 0 and returns true once it is
+bool Settler::reduceDelay()
+{
+	if (actionDelay - 1 <= 0)
+	{
+		actionDelay = 0;
 		return true;
 	}
 	else
 	{
+		actionDelay--;
 		return false;
 	}
 }
+
+// Returns the delay
+int Settler::getDelay() { return actionDelay; }
 
 Settler::~Settler() { }
--- src/settler.h
+++ src/settler.h
@@ -3,6 +3,7 @@
 
 #include <string>
 #include <vector>
+#include <queue>
 
 class Settler
 {
@@ -20,14 +21,25 @@ public:
 	bool setTask(std::string newTask);
 
 	int getHP();
-	bool setHP(int hp);
+	int removeHP(int hpChange);
+	int addHP(int hpChange);
 
+	void pushPath(int x, int y);
+	std::queue <std::pair<int, int>> getPath();
+	
+	void setDelay(int time);
+	bool reduceDelay();
+	int getDelay();
 
 private:
-	int hp;
-	std::string name;
-	std::string task;
-	std::pair<std::vector<int>, int> inventory;
+	int hp; // Current hp of the settler
+	int maxHp; // Maximum hp of the settler
+	std::string name; // Settlers name
+	std::string task; // Current task
+	std::pair<std::vector<int>, int> inventory; // Inventory, where the first element of the pair is the item identification number and the latter is the maximum size
+	std::queue<std::pair<int, int>> path; // Movement path, contains directions in the form of x and y coordinates
+	int actionDelay; // How much time action takes
+	bool playerControlled; // Determines whether settlers is able to accept commands from player or not
 };
 
 #endif
