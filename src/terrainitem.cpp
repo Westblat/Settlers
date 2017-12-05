08f0e5f33e23d8a93bac33923976456ae82f1ac7
Arthur Löfstedt <arthur.lofstedt@aalto.fi>
2017-12-05 19:40:35 +0200

    [FIX] Changed descriptions and colours of terrain

--- src/terrainitem.cpp
+++ src/terrainitem.cpp
@@ -3,24 +3,24 @@
 TerrainItem::TerrainItem(int tilesize, int type) {
 	setRect(0,0,tilesize,tilesize);
 
-	if (type == 0) {		// GRASS
-		QBrush brush = QBrush(QColor(51, 102, 0));
+	if (type == 0) {		// 0: GRASS/PLAINS
+		QBrush brush = QBrush(QColor(51, 102, 0)); // GREEN
 		setBrush(brush);
 	}
-	else if (type == 1) {	// BUILDING ???
-		QBrush brush = QBrush(QColor(51, 25, 0));
+	else if (type == 1) {	// 1: STONE
+		QBrush brush = QBrush(QColor(160, 160, 160)); // LIGHT GRAY
 		setBrush(brush);
 	}
-	else if (type == 2) {	// ROCKS ???
-		QBrush brush = QBrush(QColor(160, 160, 160));
+	else if (type == 2) {	// 2: MOUNTAIN
+		QBrush brush = QBrush(QColor(64, 64, 64));  // DARK GRAY
 		setBrush(brush);
 	}
-	else if (type == 3) {	// COAST
-		QBrush brush = QBrush(QColor(204, 204, 0));
+	else if (type == 3) {	// 3: BEACH
+		QBrush brush = QBrush(QColor(204, 204, 0)); // YELLOWISH
 		setBrush(brush);
 	}
-	else if (type == 4) {	// WATER
-		QBrush brush = QBrush(QColor(0, 76, 153));
+	else if (type == 4) {	// 4: WATER
+		QBrush brush = QBrush(QColor(0, 76, 153)); // DARK BLUE
 		setBrush(brush);
 	}
 }
