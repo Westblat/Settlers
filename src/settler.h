1f9899f6199d232047fdab3c5693a9ac47ed0fd1
JoonasLehto <joonas.eino.lehto@gmail.com>
2017-12-05 13:40:53 +0200

    [IMP]Settlers Coordinates as parameter
    Path is now stack instead of queue

--- src/settler.h
+++ src/settler.h
@@ -3,46 +3,49 @@
 
 #include <string>
 #include <vector>
-#include <queue>
+#include <stack>
 #include <algorithm>
 #include <iostream>
+#include "coordinates.h"
 
 
 class Settler
 {
 public:
-	Settler(std::string name);
+    Settler(std::string name, Coordinates location);
 	~Settler();
+
+    std::string getName();
+
+    std::string getTask();
+    bool setTask(std::string newTask);
 
 	std::vector<int> getItems();
 	bool addItem(int item);
 	bool removeItem(int item);
 
-	std::string getName();
-
-	std::string getTask();
-	bool setTask(std::string newTask);
-
 	int getHP();
 	int removeHP(int hpChange);
 	int addHP(int hpChange);
 
-	void pushPath(int x, int y);
-	std::queue <std::pair<int, int> > getPath();
+    void setPath(std::stack<std::pair<int, int> > newPath);
+    std::pair<int, int> popNextPath();
 	
 	void setDelay(int time);
 	bool reduceDelay();
 	int getDelay();
 
 private:
-	int hp; // Current hp of the settler
-	int maxHp; // Maximum hp of the settler
 	std::string name; // Settlers name
 	std::string task; // Current task
+    int hp; // Current hp of the settler
+    int maxHp; // Maximum hp of the settler
+    //Building *home;
 	std::pair<std::vector<int>, int> inventory; // Inventory, where the first element of the pair is the item identification number and the latter is the maximum size
-	std::queue<std::pair<int, int> > path; // Movement path, contains directions in the form of x and y coordinates
-	int actionDelay; // How much time action takes
-	bool playerControlled; // Determines whether settlers is able to accept commands from player or not
+    bool playerControlled; // Determines whether settlers is able to accept commands from player or not
+    Coordinates *location;
+    std::stack<std::pair<int, int> > path; // Movement path, contains directions in the form of x and y coordinates
+    int actionDelay; // How much time action takes
 };
 
 std::ostream& operator <<(std::ostream& os, Settler& settler);
