a58ac9d461cc857209c1db246ebc946c49d76b43
Joonas Malinen <joonas.malinen@hotmail.fi>
2017-12-05 13:14:19 +0200

    [IMP] Improved on building constructors

--- src/house.cpp
+++ src/house.cpp
@@ -3,6 +3,7 @@
 House::House(Terrain *terrain, bool initialize) : Building(1,10,initialize), terrain(terrain) {
     inventory.second = 0; //Sets max inventory size
     //Adds required materials to inventory, if house is not initialized as ready building
+    terrain->setBuildingType(1);
     if (!initialize){
     inventory.first.push_back(0);//TEMP? House is built with 4 "0" resources (wood)
     inventory.first.push_back(0);//UUUUUUUUUUUUUUUUUUUUUHHHHHH
