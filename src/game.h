760fec61c5a9619b864355ccd1046e721d3e699f
Joonas Malinen <joonas.malinen@hotmail.fi>
2017-12-05 15:39:26 +0200

    [FIX] Woops

--- src/game.h
+++ src/game.h
@@ -29,9 +29,10 @@ private:
     2 = cut tree
     3 = cut stone
     4 = cut iron
-    5 = empty inventory
-    6 = get item
-    7 = combat
+    5 = cut swords
+    6 = empty inventory
+    7 = get item
+    8 = combat
     */
 };
 
