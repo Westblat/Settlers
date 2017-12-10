cc3358c15f003ad3b40fcd25173d86b5c9e20f0c
JoonasLehto <joonas.eino.lehto@gmail.com>
2017-12-10 14:27:47 +0200

    [FIX] fixed missing private value and includes

--- src/road.h
+++ src/road.h
@@ -1,12 +1,16 @@
 #ifndef ROAD_H
 #define ROAD_H
 
+#include "building.h"
+#include "terrain.h"
 
 class Road : public Building
 {
 public:
     Road(Terrain* terrain, bool initialze);
     ~Road();
+private:
+    Terrain* terrain;
 };
 
 #endif // ROAD_H
