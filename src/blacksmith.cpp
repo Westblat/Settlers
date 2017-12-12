1aed9f8e9fc91eb1c065af0bfee8165afc8036a7
Joonas Malinen <joonas.malinen@hotmail.fi>
2017-12-12 16:26:40 +0200

    [IMP] Added missing stuff to blacksmith

--- src/blacksmith.cpp
+++ src/blacksmith.cpp
@@ -1,11 +1,21 @@
 #include "blacksmith.h"
 
 Blacksmith::Blacksmith(Terrain *terrain, bool initialize) : Building(5, 10, initialize), terrain(terrain) {
-	// TODO add items needed to build and set the inventory size to a correct value
-	inventory.second = 10; //Sets max inventory size
-						  //Adds required materials to inventory, if house is not initialized as ready building
-
     terrain->placeBuilding(this);
+    if(!initialize){
+        for(int i = 0; i < 10; i++){
+            inventory.first.push_back(0);
+        }
+        for(int i = 0; i < 5; i++){
+            inventory.first.push_back(1);
+        }
+        for(int i = 0; i < 5; i++){
+            inventory.first.push_back(2);
+        }
+    }
+    
+    inventory.second = 10; //Sets max inventory size
+						  //Adds required materials to inventory, if house is not initialized as ready building
 }
 
 bool Blacksmith::makeWeapon() {
